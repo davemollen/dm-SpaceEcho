9ca84fc00a291a60977124af9f37740f68ea179c
dave <davemollen@gmail.com>
2023-05-20 15:45:34 +0200

    Update lv2 gui

--- lv2/dm-SpaceEcho.lv2/modgui/javascript.js
+++ lv2/dm-SpaceEcho.lv2/modgui/javascript.js
@@ -1,16 +1,24 @@
 function(event, {set_port_value}) {
   function handle_event(symbol, value) {
     const time_link = event.icon.find("[mod-port-symbol=time_link]");
-    const is_time_linked = time_link.hasClass("active");
+    const is_time_linked = time_link.hasClass("on");
     
     switch (symbol) {
         case "time_link":
             if(value == 1) {
-              time_link.addClass("active");
+              time_link.addClass("on");
             } else {
-              time_link.removeClass("active");
+              time_link.removeClass("on");
             }
             break;
+        case "limiter":
+          const limiter = event.icon.find("[mod-port-symbol=limiter]");
+          if(value == 1) {
+            limiter.addClass("on");
+          } else {
+            limiter.removeClass("on");
+          }
+          break;
         case "time_left":
             if(is_time_linked) {
               set_port_value("time_right", value);
