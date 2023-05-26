f56f46631b2d6e52a243e305b355e69cabd2df40
Dave Mollen <dave.mollen@infi.nl>
2023-05-26 09:07:01 +0200

    Check log_smooth for subnormal values

--- space_echo/src/log_smooth.rs
+++ space_echo/src/log_smooth.rs
@@ -9,8 +9,13 @@ impl LogSmooth {
   }
 
   pub fn run(&mut self, input: f32, factor: f32) -> f32 {
-    let ad = 0.693147 / (factor * self.sample_rate).max(1.);
-    self.z = ((input - self.z) * ad) + self.z;
-    self.z
+    let difference = input - self.z;
+    if difference.is_subnormal() {
+      input
+    } else {
+      let ad = 0.693147 / (factor * self.sample_rate).max(1.);
+      self.z = (difference * ad) + self.z;
+      self.z
+    }
   }
 }
