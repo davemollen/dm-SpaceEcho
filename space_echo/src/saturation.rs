bbd9a5ad63375cd0c33f6153aed9db0fe94d98f9
Dave Mollen <davemollen@gmail.com>
2024-08-04 09:20:50 +0200

    Cheaper saturation

--- space_echo/src/saturation.rs
+++ space_echo/src/saturation.rs
@@ -1,25 +1,22 @@
-use crate::FloatExt;
-
 pub struct Saturation;
 
 impl Saturation {
   pub fn process(input: (f32, f32), mix: f32) -> (f32, f32) {
     let mix = mix.clamp(0., 1.);
-    let saturated = Self::fast_atan2(input);
 
     (
-      input.0 + (saturated.0 - input.0) * mix,
-      input.1 + (saturated.1 - input.1) * mix,
+      input.0 + (Self::saturate(input.0) - input.0) * mix,
+      input.1 + (Self::saturate(input.1) - input.1) * mix,
     )
   }
 
-  fn fast_atan2(x: (f32, f32)) -> (f32, f32) {
-    let n1 = 0.97239411;
-    let n2 = -0.19194795;
-
-    (
-      ((n1 + n2 * x.0 * x.0) * x.0).clamp(-1., 1.),
-      ((n1 + n2 * x.1 * x.1) * x.1).clamp(-1., 1.),
-    )
+  fn saturate(x: f32) -> f32 {
+    if x < -2.65155 {
+      1.
+    } else if x > 2.65155 {
+      -1.
+    } else {
+      (0.97239411 - 0.19194795 * x * x) * x
+    }
   }
 }
