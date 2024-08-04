e65ab50bf14c94a55e5e39e1fbcfea1348f9362e
Dave Mollen <davemollen@gmail.com>
2024-08-04 10:45:00 +0200

    Square saturation mix

--- space_echo/src/saturation.rs
+++ space_echo/src/saturation.rs
@@ -2,7 +2,7 @@ pub struct Saturation;
 
 impl Saturation {
   pub fn process(input: (f32, f32), mix: f32) -> (f32, f32) {
-    let mix = mix.clamp(0., 1.);
+    let mix = (mix * mix).clamp(0., 1.);
 
     (
       input.0 + (Self::saturate(input.0) - input.0) * mix,
