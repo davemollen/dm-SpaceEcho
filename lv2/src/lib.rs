d4d6f0c3f6db3579f4467435dbd4ac123f295cd7
davemollen <davemollen@gmail.com>
2025-07-21 08:26:10 +0200

    Allow in-place processing

--- lv2/src/lib.rs
+++ lv2/src/lib.rs
@@ -5,30 +5,30 @@ use space_echo::{Params, SpaceEcho};
 
 #[derive(PortCollection)]
 struct Ports {
-  input: InputPort<Control>,
-  time_link: InputPort<Control>,
-  time_left: InputPort<Control>,
-  time_right: InputPort<Control>,
-  feedback: InputPort<Control>,
-  hold: InputPort<Control>,
-  wow_and_flutter: InputPort<Control>,
-  channel_mode: InputPort<Control>,
-  time_mode: InputPort<Control>,
-  highpass_freq: InputPort<Control>,
-  highpass_res: InputPort<Control>,
-  lowpass_freq: InputPort<Control>,
-  lowpass_res: InputPort<Control>,
-  reverb: InputPort<Control>,
-  decay: InputPort<Control>,
-  stereo: InputPort<Control>,
-  duck: InputPort<Control>,
-  output: InputPort<Control>,
-  mix: InputPort<Control>,
-  limiter: InputPort<Control>,
-  input_left: InputPort<Audio>,
-  input_right: InputPort<Audio>,
-  output_left: OutputPort<Audio>,
-  output_right: OutputPort<Audio>,
+  input: InputPort<InPlaceControl>,
+  time_link: InputPort<InPlaceControl>,
+  time_left: InputPort<InPlaceControl>,
+  time_right: InputPort<InPlaceControl>,
+  feedback: InputPort<InPlaceControl>,
+  hold: InputPort<InPlaceControl>,
+  wow_and_flutter: InputPort<InPlaceControl>,
+  channel_mode: InputPort<InPlaceControl>,
+  time_mode: InputPort<InPlaceControl>,
+  highpass_freq: InputPort<InPlaceControl>,
+  highpass_res: InputPort<InPlaceControl>,
+  lowpass_freq: InputPort<InPlaceControl>,
+  lowpass_res: InputPort<InPlaceControl>,
+  reverb: InputPort<InPlaceControl>,
+  decay: InputPort<InPlaceControl>,
+  stereo: InputPort<InPlaceControl>,
+  duck: InputPort<InPlaceControl>,
+  output: InputPort<InPlaceControl>,
+  mix: InputPort<InPlaceControl>,
+  limiter: InputPort<InPlaceControl>,
+  input_left: InputPort<InPlaceAudio>,
+  input_right: InputPort<InPlaceAudio>,
+  output_left: OutputPort<InPlaceAudio>,
+  output_right: OutputPort<InPlaceAudio>,
 }
 
 #[uri("https://github.com/davemollen/dm-SpaceEcho")]
@@ -59,40 +59,39 @@ impl Plugin for DmSpaceEcho {
   // iterates over.
   fn run(&mut self, ports: &mut Ports, _features: &mut (), _sample_count: u32) {
     self.params.set(
-      *ports.input,
-      *ports.channel_mode as i32 - 1,
-      *ports.time_mode as i32 - 1,
-      *ports.time_link == 1.,
-      *ports.time_left,
-      *ports.time_right,
-      *ports.feedback * 0.01,
-      *ports.wow_and_flutter * 0.01,
-      *ports.highpass_freq,
-      *ports.highpass_res * 0.01,
-      *ports.lowpass_freq,
-      *ports.lowpass_res * 0.01,
-      *ports.reverb * 0.01,
-      *ports.decay * 0.005,
-      *ports.stereo * 0.01,
-      *ports.duck * 0.01,
-      *ports.output,
-      *ports.mix * 0.01,
-      *ports.limiter == 1.,
-      *ports.hold == 1.,
+      ports.input.get(),
+      ports.channel_mode.get() as i32 - 1,
+      ports.time_mode.get() as i32 - 1,
+      ports.time_link.get() == 1.,
+      ports.time_left.get(),
+      ports.time_right.get(),
+      ports.feedback.get() * 0.01,
+      ports.wow_and_flutter.get() * 0.01,
+      ports.highpass_freq.get(),
+      ports.highpass_res.get() * 0.01,
+      ports.lowpass_freq.get(),
+      ports.lowpass_res.get() * 0.01,
+      ports.reverb.get() * 0.01,
+      ports.decay.get() * 0.005,
+      ports.stereo.get() * 0.01,
+      ports.duck.get() * 0.01,
+      ports.output.get(),
+      ports.mix.get() * 0.01,
+      ports.limiter.get() == 1.,
+      ports.hold.get() == 1.,
     );
 
     let input_channels = ports.input_left.iter().zip(ports.input_right.iter());
-    let output_channels = ports
-      .output_left
-      .iter_mut()
-      .zip(ports.output_right.iter_mut());
+    let output_channels = ports.output_left.iter().zip(ports.output_right.iter());
 
     for ((input_left, input_right), (output_left, output_right)) in
       input_channels.zip(output_channels)
     {
-      (*output_left, *output_right) = self
+      let output = self
         .space_echo
-        .process((*input_left, *input_right), &mut self.params);
+        .process((input_left.get(), input_right.get()), &mut self.params);
+      output_left.set(output.0);
+      output_right.set(output.1);
     }
   }
 }
